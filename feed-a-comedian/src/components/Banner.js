5b1f5cbfbfae5ab5690ac8d85ad3e6eab25ab994
soondubusoup <84359392+soondubusoup@users.noreply.github.com>
2022-12-12 18:53:57 -0600

    banner added

--- feed-a-comedian/src/components/Banner.js
+++ feed-a-comedian/src/components/Banner.js
@@ -1,25 +1,63 @@
-import {useState, useEffect} from "react"
-import { Container, Row, Col} from 'react-bootstrap'
-import { ArrowRightCircle} from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg"
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+
 
 export const Banner = () => {
-    return (
-        <section className="banner" id="home"> 
-          <Container>
-            <row className="align-items-center">
-                <col xs={12} md={6} xl={7}>
-                    <span className="tagline"> Welcome to my Portfolio</span>
-                    <h1>{`Hi I'm Jimmy`}<span className="wrap">web developer</span></h1>
-                    <p>Searching for One Piece</p>
-                    <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25}/></button>
-                </col>
-                <col xs={12} md={6} xl={5}>
-                    <img src={headerImg} alt="Headder Img" />
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
 
-                </col>
-            </row>
-          </Container>
-        </section>
-    )
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+          
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+           
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
 }
