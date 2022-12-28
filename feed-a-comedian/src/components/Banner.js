2e112977f1eaa8916d09daaa1269d0e86b2401dd
soondubusoup <84359392+soondubusoup@users.noreply.github.com>
2022-12-28 10:14:40 -0600

    added contact

--- feed-a-comedian/src/components/Banner.js
+++ feed-a-comedian/src/components/Banner.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
 
 
 export const Banner = () => {
